34e982f15d3285a3a8edb5920d0867768dfdb34b
Denish Borad <denish5106@gmail.com>
2024-05-25 12:45:19 +0530

    make models and database connection

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-const express = require("express");
-
-
+require('dotenv').config();
+const express = require('express')
+const bodyparse = require('body-parser')
+const User = require('./models/user_model')
+// const sequelize = require('./connection')
 const app = express();
 
+app.use(bodyparse.urlencoded({ extended: true }));
+app.use(bodyparse.json());
+
+// User.drop()
+
+// particular models create use
+// User.sync()
 
 
+// Force Fully Create Table
+// User.sync({ force: true })
 
+// Changes Update in Table 
+// Also use this 
+User.sync({ alter: true })
+
+// all existing models create use
+// sequelize.sync({ force: true })
+
+// Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server running on port ${PORT}`);
+    }
+});
---
+++ models/user_model.js
@@ -0,0 +1,77 @@
+// Model Definition
+// Extending Model and calling init(attributes, options)
+
+const { DataTypes, Model } = require('sequelize');
+const sequelize =  require('../connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      // allowNull defaults to true
+    },
+  },
+  {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'users', // We need to choose the model name
+  },
+);
+
+// the defined model is the class itself
+console.log(User === sequelize.models.User); // true
+
+module.exports = User
+
+
+
+
+
+// Model Definition
+// Calling sequelize.define(modelName, attributes, options)
+
+
+
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../connection');
+
+// const User = sequelize.define(
+//   'User',
+//   {
+//     firstName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     lastName: {
+//       type: DataTypes.STRING,
+//     },
+//   },
+//   {
+//     // Other model options go here
+//     tableName: 'users',
+
+//     // This Remove Buil-In CreateAt and UpdatedAt Field
+//     // timestamps: false,
+
+//     // don't forget to enable timestamps!
+//     timestamps: true,
+
+//     // I don't want createdAt
+//     createdAt: false,
+
+//     // I want updatedAt to actually be called updateTimestamp
+//     updatedAt: 'updated_at',
+//   },
+// );
+
+// // `sequelize.define` also returns the model
+// console.log(User === sequelize.models.User); // true
+
+// module.exports = User
